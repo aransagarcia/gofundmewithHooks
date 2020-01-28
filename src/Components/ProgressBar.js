a91c405347ba6de481925cf9a49794f277221560
aransagarcia <aransagarcia@pursuit.org>
2020-01-28 01:01:24 -0500

    create progress bar component

---
+++ src/Components/ProgressBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <div id="myProgress">
+      <div className="myBar">10%</div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "../App.css";
+import ProgressBar from "./ProgressBar";
+
 
 const RecentDonations = props => {
   const donationList = props.RecentDonation.map(el => {
     return (
-      <div key= {el.name} >
-        <h3> {el.name} donated {el.inputAmount} </h3>
+      <div key={el.name}>
+        <h3>
+          {" "}
+          {el.name} donated {el.inputAmount}{" "}
+        </h3>
         <p> {el.caption} </p>
       </div>
     );
@@ -15,6 +20,7 @@ const RecentDonations = props => {
     <div className="Recent Donations">
       <div className="List">
         <h1> Recent Donations </h1>
+    <ProgressBar/>
         <ul>{donationList}</ul>
       </div>
     </div>
